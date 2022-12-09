bef910bfc3264330a6d231a4fce81c47cc01c35f
Jay Shah <54587542+jay1299@users.noreply.github.com>
2022-12-09 14:01:07 +0530

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'ap-south-1_OMSzpcCr6', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '271aool4h726u4agl2tgmdl1e1', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-south-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://1801wlvxn7.execute-api.ap-south-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
